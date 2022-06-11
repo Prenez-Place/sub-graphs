e47eb27fe3dd58c00b1b2150293fd38b1030ad41
PhilippeMts <phi.metais@gmail.com>
2022-06-11 11:38:49 +0200

    ✨ Introduce sub-graphs to index keywords store for Prenez Place.

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,46 +1,28 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, store } from "@graphprotocol/graph-ts"
 import { Store, Added, Removed } from "../generated/Store/Store"
-import { ExampleEntity } from "../generated/schema"
+import { Fragment } from "../generated/schema"
 
 export function handleAdded(event: Added): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let id = event.params.fragment
+  let entity = Fragment.load(id)
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new Fragment(id)
+    entity.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.fragment = event.params.fragment
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
 }
 
-export function handleRemoved(event: Removed): void {}
+export function handleRemoved(event: Removed): void {
+  let id = event.params.fragment
+  let entity = Fragment.load(id)
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity) {
+    store.remove('Fragment', id)
+  }
+}
